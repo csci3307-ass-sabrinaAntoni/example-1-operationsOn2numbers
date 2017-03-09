be555c9f789035a5cb4c1d7105313662ed8ecbca
Sabrina <Sabrina@132.238.220.21>
2017-03-08 20:01:41 -0500

    initial commit

---
+++ src/com/mycompany/basicmathoperations/simple/Divide.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.mycompany.basicmathoperations.simple;
+
+/**
+ * @author Sabrina
+ *
+ */
+public class Divide {
+	/**
+	 * To divide 2 int numbers
+	 * @return num1 / num2
+	 */
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+}
